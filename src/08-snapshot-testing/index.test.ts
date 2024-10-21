743f41303b0c5fddafe21e1f97739b152b664a11
sergkrav.social@gmail.com <sergkrav.social@gmail.com>
2024-10-22 04:32:03 +0700

    feat: finish snapshot testing

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,25 @@
 // Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
 
 describe('generateLinkedList', () => {
-  // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    const values = [1, 2];
+    const expectedList = {
+      value: 1,
+      next: {
+        value: 2,
+        next: {
+          value: null,
+          next: null,
+        },
+      },
+    };
+    expect(generateLinkedList(values)).toEqual(expectedList);
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    const values = [1, 2];
+    expect(generateLinkedList(values)).toMatchSnapshot();
   });
 });
